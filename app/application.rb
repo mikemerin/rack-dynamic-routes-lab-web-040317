7987235556cdec15364a258c7120e9e7ee78a45e
Mike Merin <mikemerinweather@gmail.com>
2017-04-24 11:39:38 -0400

    Done.

---
+++ app/application.rb
@@ -0,0 +1,35 @@
+class Application
+
+  @@items = []
+
+  # Item.new("Figs",3.42),
+  # Item.new("Pears",0.99)
+
+  attr_accessor :item
+
+  def call(env)
+
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+
+    if req.path.match(/items/)
+      item = req.path.split("/items/").last
+      item_info = @@items.find { |i| i.name == item }
+      
+      if item_info == nil
+        resp.write "Item not found"
+        resp.status = 400
+      else
+        resp.write "#{item_info.price}"
+        resp.status = 200
+      end
+    else
+      resp.write "Route not found"
+      resp.status = 404
+    end
+
+    resp.finish
+
+  end
+
+end
