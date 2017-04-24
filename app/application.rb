f5ec71c558125081b28bb7b1ccce7ee5f26b49ff
Mike Merin <mikemerinweather@gmail.com>
2017-04-24 11:41:10 -0400

    Done.

--- app/application.rb
+++ app/application.rb
@@ -2,9 +2,6 @@ class Application
 
   @@items = []
 
-  # Item.new("Figs",3.42),
-  # Item.new("Pears",0.99)
-
   attr_accessor :item
 
   def call(env)
@@ -15,7 +12,7 @@ class Application
     if req.path.match(/items/)
       item = req.path.split("/items/").last
       item_info = @@items.find { |i| i.name == item }
-      
+
       if item_info == nil
         resp.write "Item not found"
         resp.status = 400
@@ -23,9 +20,11 @@ class Application
         resp.write "#{item_info.price}"
         resp.status = 200
       end
+
     else
       resp.write "Route not found"
       resp.status = 404
+
     end
 
     resp.finish
